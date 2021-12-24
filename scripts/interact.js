2c4281fb20e11b56b52c713ba0eab658384870ab
Hideto Yasunori <hideto0118yasunori@gmail.com>
2021-12-24 14:59:04 -0800

    Add provider, signer and contract

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,3 +1,17 @@
 const API_KEY = process.env.API_KEY;
 const PREVIOUS_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const { ethers } = require("hardhat");
+// FFOr Hardhat
+const contract =  require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+console.log(JSON.stringify(contract.abi));
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemmyProvider(network="ropsten", API_KEY);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
